7ea76f97d40536c80147aa84cb8880bf979b0114
Sravanthi Palakonda <srapal@codeaurora.org>
2015-07-23 19:39:05 +0530

    WiFi: Do not use random MAC address for scan during WPS session.
    Probe requests with the random MAC address during the WPS PBC session
    shall result in to the overlap detection and hence a WPS failure. Thus,
    instruct the host driver to not randomize the MAC addresses during the WPS
    session by clearing the MAC at the start of WPS session. MAC address
    randomization shall again gets resumed after the termination of WPS
    operation( either success / failure).
    
    Change-Id: Ie03d9c05a28654ecd429ce8a1c955e71d7828274

--- service/java/com/android/server/wifi/WifiStateMachine.java
+++ service/java/com/android/server/wifi/WifiStateMachine.java
@@ -317,6 +317,7 @@ public class WifiStateMachine extends StateMachine implements WifiNative.WifiPno
     private boolean mEnableRssiPolling = false;
     private boolean mLegacyPnoEnabled = false;
     private boolean mDisabled5GhzFrequencies = false;
+    private boolean mIsRandomMacCleared = false;
     private int mRssiPollToken = 0;
     /* 3 operational states for STA operation: CONNECT_MODE, SCAN_ONLY_MODE, SCAN_ONLY_WIFI_OFF_MODE
     * In CONNECT_MODE, the STA can scan and connect to an access point
@@ -1620,6 +1621,11 @@ public class WifiStateMachine extends StateMachine implements WifiNative.WifiPno
         ouiBytes[2] = (byte) (Integer.parseInt(ouiParts[2], 16) & 0xFF);
 
         logd("Setting OUI to " + oui);
+        return mWifiNative.setScanningMacOui(ouiBytes);
+    }
+    private boolean clearRandomMacOui() {
+        byte[] ouiBytes = new byte[]{0,0,0};
+        logd("Clear random OUI");
         return mWifiNative.setScanningMacOui(ouiBytes);
     }
 
@@ -7936,6 +7942,8 @@ public class WifiStateMachine extends StateMachine implements WifiNative.WifiPno
                     WpsResult wpsResult;
                     switch (wpsInfo.setup) {
                         case WpsInfo.PBC:
+                            clearRandomMacOui();
+                            mIsRandomMacCleared = true;
                             wpsResult = mWifiConfigStore.startWpsPbc(wpsInfo);
                             break;
                         case WpsInfo.KEYPAD:
@@ -9743,6 +9751,10 @@ public class WifiStateMachine extends StateMachine implements WifiNative.WifiPno
         public void exit() {
             mWifiConfigStore.enableAllNetworks();
             mWifiConfigStore.loadConfiguredNetworks();
+            if (mIsRandomMacCleared) {
+                setRandomMacOui();
+                mIsRandomMacCleared = false;
+            }
         }
     }
 
