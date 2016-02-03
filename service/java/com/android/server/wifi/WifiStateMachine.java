d4cdcc7dcf75c6166c1d33a4dd67e0a2cdaf4262
Nalla Kartheek <karthe@codeaurora.org>
2016-02-03 14:13:15 +0530

    Wifi: Unblacklist the BSSIDs, if driver roams to the same again.
    Auto join black list the connected BSSIDs, if driver roams away from it.
    This shall result in the respective BSSID getting blacklisted for the next
    strategy. Enhance this logic by un blacklisting such BSSID's if the driver
    has roamed again. This goes with our logic that - driver roaming to such
    BSSID again should indicate to the framework that the BSSID is the best in
    the network.
    
    Change-Id: I24bf6caeb9a9277db577d0870e6961609ebf7677
    CRs-Fixed: 968421

--- service/java/com/android/server/wifi/WifiStateMachine.java
+++ service/java/com/android/server/wifi/WifiStateMachine.java
@@ -9150,6 +9150,7 @@ public class WifiStateMachine extends StateMachine implements WifiNative.WifiPno
                     if (toBSSID != null && !toBSSID.equals(mWifiInfo.getBSSID())) {
                         mWifiConfigStore.driverRoamedFrom(mWifiInfo);
                     }
+                    mWifiConfigStore.unblackListDriverRoamedBSSID(toBSSID);
                     return NOT_HANDLED;
                 case WifiMonitor.NETWORK_DISCONNECTION_EVENT:
                     long lastRoam = 0;
